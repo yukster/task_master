9aef81f48393403b65e5ac42d6877a8ec579ef0c
Ben Munat <bmunat@gmail.com>
2026-06-13 22:11:08 -0700

    Initial Task Attempt work and some temp notes

--- lib/task_master/tasks.ex
+++ lib/task_master/tasks.ex
@@ -6,6 +6,24 @@ defmodule TaskMaster.Tasks do
   alias TaskMaster.Repo
   alias TaskMaster.Tasks.Task
 
+  # add task lifecycle functions here
+  # state machine is enforced by lifecycle functions
+  # this also inserts and updates Attempts
+  # lifecycle also results in summary metrics updating
+  # which should invalidate the cache
+
+  # create_task inserts the Task record and the Oban job; status defaults to :queued
+  # start_task updates the Task status to :processing and inserts an Attempt with started_at
+
+  # if attempt succeeds, complete_task updates the Task status to :completed
+  # and updates the Attempt with ended_at and result
+  # if attempt fails, fail_task updates the Task status back to :queued
+  # and inserts an new Oban job for the next Attempt
+  # (I kinda feel like I'm reimplementing Oban here)
+
+  # these are the functions the Oban job can call; need throrough tests around these
+  ##
+
   @doc """
   Returns the list of tasks.
 
@@ -52,9 +70,10 @@ defmodule TaskMaster.Tasks do
       {:error, %Ecto.Changeset{}}
 
   """
+  # this will need to insert the oban job as well, in a txn/multi
   def create_task(attrs) do
     %Task{}
-    |> Task.changeset(attrs)
+    |> Task.create_changeset(attrs)
     |> Repo.insert()
   end
 
@@ -69,22 +88,10 @@ defmodule TaskMaster.Tasks do
       {:error, %Ecto.Changeset{}}
 
   """
+  # under the covers, this needs to handle the attempt tracking too
   def update_task(%Task{} = task, attrs) do
     task
-    |> Task.changeset(attrs)
+    |> Task.update_changeset(attrs)
     |> Repo.update()
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking task changes.
-
-  ## Examples
-
-      iex> change_task(task)
-      %Ecto.Changeset{data: %Task{}}
-
-  """
-  def change_task(%Task{} = task, attrs \\ %{}) do
-    Task.changeset(task, attrs)
   end
 end
--- lib/task_master/tasks/task.ex
+++ lib/task_master/tasks/task.ex
@@ -10,6 +10,17 @@ defmodule TaskMaster.Tasks.Task do
   @task_statuses [:queued, :processing, :completed, :failed]
   @all_fields [:title, :type, :priority, :status, :payload, :max_attempts]
 
+  defmodule Attempt do
+    use Ecto.Schema
+
+    embedded_schema do
+      field :started_at, :utc_datetime_usec
+      field :ended_at, :utc_datetime_usec
+      field :result, Ecto.Enum, values: [:completed, :failed]
+      field :error, :string
+    end
+  end
+
   schema "tasks" do
     field :title, :string
     field :type, Ecto.Enum, values: @task_types
@@ -18,11 +29,13 @@ defmodule TaskMaster.Tasks.Task do
     field :payload, :map
     field :max_attempts, :integer, default: 3
 
+    embeds_many :attempts, Attempt
+
     timestamps(type: :utc_datetime_usec)
   end
 
   @doc false
-  def changeset(task, attrs) do
+  def create_changeset(task, attrs) do
     task
     |> cast(attrs, @all_fields)
     |> validate_required(@all_fields)
@@ -39,5 +52,27 @@ defmodule TaskMaster.Tasks.Task do
           []
       end
     end)
+  end
+
+  def update_changeset(task, attrs) do
+    task
+    |> cast(attrs, [:status])
+    |> validate_required([:status])
+    |> cast_embed(:attempts, with: &attempt_changeset/2)
+  end
+
+  defp attempt_changeset(attempt, attrs) do
+    attempt
+    |> cast(attrs, [:started_at, :ended_at, :result, :error])
+    |> validate_required([:started_at])
+    |> validate_completion()
+  end
+
+  defp validate_completion(changeset) do
+    case get_field(changeset, :result) do
+      # still in progress, ended_at not required yet
+      nil -> changeset
+      _ -> validate_required(changeset, [:ended_at, :result])
+    end
   end
 end
